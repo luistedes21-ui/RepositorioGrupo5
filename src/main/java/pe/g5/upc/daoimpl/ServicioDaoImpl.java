3b2c81db55a8e7da65b91ff6bd55ec7acd095c52
Anthamy22 <anthamybarreraalarcon@gmail.com>
2021-04-22 01:48:50 -0500

    backend servicio

---
+++ src/main/java/pe/g5/upc/daoimpl/ServicioDaoImpl.java
@@ -0,0 +1,43 @@
+package pe.g5.upc.daoimpl;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import pe.g5.upc.dao.iServicioDao;
+import pe.g5.upc.entity.Servicio;
+
+public class ServicioDaoImpl implements iServicioDao, Serializable {
+	private static final long serialVersionUID=1L;
+	
+	@PersistenceContext(unitName = "a")
+	private EntityManager em;
+	
+	@Transactional
+	@Override
+	public void insertar(Servicio servicio) {
+		em.persist(servicio);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Servicio> listar() {
+		List<Servicio> lista = new ArrayList<Servicio>();
+		Query q = em.createQuery("select m from Servicio m");
+		lista = (List<Servicio>) q.getResultList();
+		return lista;
+	}
+
+	@Override
+	public void eliminar(int idServicio) {
+		Servicio servicio = new Servicio();
+		servicio = em.getReference(Servicio.class, idServicio);
+		em.remove(servicio);
+		
+	}
+}
